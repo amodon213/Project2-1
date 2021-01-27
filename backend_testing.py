b859a37fd38d4bfa78e5b1cc70946f56590ad9ea
keidar <53145920+keidar@users.noreply.github.com>
2021-01-27 16:51:45 +0200

    Update backend_testing.py

--- backend_testing.py
+++ backend_testing.py
@@ -1,25 +1,22 @@
 import requests
 from db_connector import *
 
-# try:
-#Post new user data to DB
-res = requests.post('http://127.0.0.1:5000/users/6666', json={"user_name": "Dvir"})
+try:
+    #Post new user data to DB
+    res = requests.post('http://127.0.0.1:5000/users/6666', json={"user_name": "Dvir"})
 
-#submit that new user inserted to DB
-check_insert = requests.get('http://127.0.0.1:5000/users/6666')
+    #submit that new user inserted to DB
+    check_insert = requests.get('http://127.0.0.1:5000/users/6666')
 
-#show all stored data on DB table
-table = get_table()
-table=json.loads(table)
+    #show all stored data on DB table
+    table = get_table()
+    table=json.loads(table)
 
 
-#print results
-print(type(res))
-print(res.text)
-print(res)
-print(res.json())
-print(check_insert.json())
-for i in table:
-    print(i)
-# except:
-#     print("test failed")
+    #print results
+    print(res.json())
+    print(check_insert.json())
+    for i in table:
+        print(i)
+except:
+    print("test failed")
