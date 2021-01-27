b0dd9dd972d0217abec5b2776983b450904cdd68
keidar <53145920+keidar@users.noreply.github.com>
2021-01-27 12:18:27 +0200

    Add files via upload

---
+++ clean_environment.py
@@ -0,0 +1,6 @@
+import requests
+try:
+    requests.get('http://127.0.0.1:5000/stop_server',timeout=5)
+    requests.get('http://127.0.0.1:5001/stop_server',timeout=5)
+except requests.exceptions.ReadTimeout:
+    print("The request timeout -  504 error")
---
+++ web_app.py
@@ -0,0 +1,26 @@
+import os
+import signal
+from flask import Flask
+from db_connector import *
+app = Flask(__name__)
+
+
+# supported methods
+@app.route('/users/get_user_data/<user_id>')
+def user(user_id):
+    try:
+        name = get_user_id(user_id)
+        return "<H1 id='user'>" + name + "</H1>", 200
+    except:
+        return "<H1 id='error'>""no such user: " + user_id + "</H1>", 500
+
+@app.route('/stop_server')
+def stop_server():
+    try:
+        os.kill(os.getpid(), signal.CTRL_C_EVENT)
+        return 'Server stopped',200
+    except:
+        return {'status': 'error', 'reason': "didn't manage to close rest app'"}, 500
+
+
+app.run(host='127.0.0.1', debug=True, port=5001)
