d05bc45a273a78d5052b3357dca2cc00e28db167
keidarb <keidar11@gmail.com>
2021-01-30 14:16:52 +0200

    Update rest_app.py

--- rest_app.py
+++ rest_app.py
@@ -70,4 +70,4 @@ def stop_server():
         return {'status': 'error', 'reason': "didn't manage to close rest app'"}, 500
 
 
-app.run(host='127.0.0.1', debug=True, port=5000)
+app.run(host='0.0.0.0', debug=True, port=5000)
