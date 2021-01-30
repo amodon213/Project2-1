def858570865b409c6f4755c7f4d29e4cefd8b08
keidarb <keidar11@gmail.com>
2021-01-30 14:20:22 +0200

    Update backend_testing.py

--- backend_testing.py
+++ backend_testing.py
@@ -3,11 +3,11 @@ from db_connector import *
 
 try:
 #Post new user data to DB
-    res = requests.post('http://127.0.0.1:5000/users/6666', json={"user_name": "Dvir"})
-
+    #res = requests.post('http://127.0.0.1:5000/users/6666', json={"user_name": "Dvir"})
+    res = requests.post('http://0.0.0.0:5000/users/6666', json={"user_name": "Dvir"})
     #submit that new user inserted to DB
-    check_insert = requests.get('http://127.0.0.1:5000/users/6666')
-
+    #check_insert = requests.get('http://127.0.0.1:5000/users/6666')
+    check_insert = requests.get('http://0.0.0.0:5000/users/6666')
     #show all stored data on DB table
     table = get_table()
     table=json.loads(table)
