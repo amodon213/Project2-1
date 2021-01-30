ca511cae63da6c1ada4b8f8987467292ea337a62
keidarb <keidar11@gmail.com>
2021-01-30 14:22:17 +0200

    Update backend_testing.py

--- backend_testing.py
+++ backend_testing.py
@@ -4,7 +4,7 @@ from db_connector import *
 try:
 #Post new user data to DB
     #res = requests.post('http://127.0.0.1:5000/users/6666', json={"user_name": "Dvir"})
-    res = requests.post('http://0.0.0.0:5000/users/6666', json={"user_name": "Dvir"})
+    # res = requests.post('http://0.0.0.0:5000/users/6666', json={"user_name": "Dvir"})
     #submit that new user inserted to DB
     #check_insert = requests.get('http://127.0.0.1:5000/users/6666')
     check_insert = requests.get('http://0.0.0.0:5000/users/6666')
@@ -14,7 +14,7 @@ try:
 
 
     #print results
-    print(res.json())
+    # print(res.json())
     print(check_insert.json())
     for i in table:
         print(i)
